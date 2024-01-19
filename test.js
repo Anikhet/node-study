1fa4363061a0cced4f3d557c2e052c0eddb99b48
Anikhet Mulky <am9559@rit.edu>
2024-01-19 00:47:51 -0800

    few topics

--- test.js
+++ test.js
@@ -1,2 +1,20 @@
-const name = 'mario';
-console.log(name);
+
+
+// time out and interval
+
+// setTimeout(()=> {
+//     console.log('in the Matrix after 3 seconds')
+//     clearInterval(inter)
+// },3000);
+
+// const inter = setInterval(()=> {
+//     console.log('in the Matrix every 1 seconds')
+// },1000);
+
+
+// Directory and file name
+
+console.log(__dirname);
+
+console.log(__filename);
+
